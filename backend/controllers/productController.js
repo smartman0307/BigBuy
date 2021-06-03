e43b5e614e5723846089822c586c064c438284c2
Haruto0307 <skybolt0307@gmail.com>
2021-06-03 15:47:52 +0530

    Create Product Model and create new product in database

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,13 @@
+const Product = require('../models/product')
+
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body)
+  res.status(201).json({
+    success: true,
+    product,
+  })
+}
+
 exports.getProducts = (req, res, next) => {
   res.status(200).json({
     success: true,
