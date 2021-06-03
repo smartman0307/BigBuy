a8e36b399c467b035f7459391e30a4b3779025da
Haruto0307 <skybolt0307@gmail.com>
2021-06-03 17:44:21 +0530

    Create,Update and Delete ProductController

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,3 +1,4 @@
+const { listeners } = require('../models/product')
 const Product = require('../models/product')
 
 exports.newProduct = async (req, res, next) => {
@@ -8,9 +9,69 @@ exports.newProduct = async (req, res, next) => {
   })
 }
 
-exports.getProducts = (req, res, next) => {
+exports.getProducts = async (req, res, next) => {
+  const products = await Product.find()
+
   res.status(200).json({
     success: true,
-    message: 'Products in the Database',
+    count: products.length,
+    products,
+  })
+}
+
+exports.getSingleProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id)
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found',
+    })
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  })
+}
+
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id)
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found',
+    })
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  })
+
+  res.status(200).json({
+    success: true,
+    product,
+  })
+}
+
+exports.deleteProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id)
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: 'Product not found',
+    })
+  }
+
+  // product = await Product.findByIdAndDelete(req.params.id)
+  await product.remove()
+
+  res.status(200).json({
+    success: true,
+    message: `Product ${product.id} Deleted`,
   })
 }
