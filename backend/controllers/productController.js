17f45ddae2886b613a09c39d96b2b6a51c840eff
Haruto0307 <skybolt0307@gmail.com>
2021-06-14 17:25:59 +0530

    Add user to product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,10 @@ const catchAsyncErrors = require('../middlewares/catchAsyncErrors')
 const APIFeatures = require('../utils/apiFeatures')
 
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
+  req.body.user = req.user.id
+
   const product = await Product.create(req.body)
+
   res.status(201).json({
     success: true,
     product,
