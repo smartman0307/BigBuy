1299a7f4ef9bf8423029ffb5553163418bb68952
Haruto0307 <skybolt0307@gmail.com>
2021-07-07 13:56:25 +0530

    Register User actions and reducers

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,6 +4,9 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   CLEAR_ERRORS,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
 } from '../constants/userConstants'
 
 // Login
@@ -30,6 +33,31 @@ export const login = (email, password) => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: LOGIN_FAIL,
+      payload: error.response.data.message,
+    })
+  }
+}
+
+//Register
+export const register = (userData) => async (dispatch) => {
+  try {
+    dispatch({ type: REGISTER_USER_REQUEST })
+
+    const config = {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    }
+
+    const { data } = await axios.post('/api/v1/register', userData, config)
+
+    dispatch({
+      type: REGISTER_USER_SUCCESS,
+      payload: data.user,
+    })
+  } catch (error) {
+    dispatch({
+      type: REGISTER_USER_FAIL,
       payload: error.response.data.message,
     })
   }
