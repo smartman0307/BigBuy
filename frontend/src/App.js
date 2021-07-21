9538a9fc4181200d79f900437ef513d1e7a9a836
Haruto0307 <skybolt0307@gmail.com>
2021-07-21 23:55:19 +0530

    Create Admin Dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import axios from 'axios'
 import './index.css'
+
 import Header from './components/layouts/Header'
 import Footer from './components/layouts/Footer'
+
 import Home from './components/Home'
 import ProductDetails from './components/products/ProductDetails'
+
 import Login from './components/user/Login'
 import Register from './components/user/Register'
 import Profile from './components/user/Profile'
@@ -13,12 +16,15 @@ import UpdateProfile from './components/user/UpdateProfile'
 import UpdatePassword from './components/user/UpdatePassword'
 import ForgotPassword from './components/user/ForgotPassword'
 import ResetPassword from './components/user/ResetPassword'
+
 import ProtectedRoute from './components/route/ProtectedRoute'
+
 import Cart from './components/cart/Cart'
 import Shipping from './components/cart/Shipping'
 import ConfirmOrder from './components/cart/ConfirmOrder'
 import Payment from './components/cart/Payment'
 import OrderSuccess from './components/cart/OrderSuccess'
+
 import ListOrders from './components/order/ListOrders'
 import OrderDetails from './components/order/OrderDetails'
 
@@ -28,6 +34,8 @@ import store from './store'
 // Payment
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
+
+import Dashboard from './components/admin/Dashboard'
 
 function App() {
   const [stripeApiKey, setStripeApiKey] = useState('')
@@ -63,9 +71,9 @@ function App() {
           />
           <ProtectedRoute path='/profile' component={Profile} exact />
           <ProtectedRoute path='/shipping' component={Shipping} />
-          <ProtectedRoute path='/order/confirm' component={ConfirmOrder} />
           <ProtectedRoute path='/success' component={OrderSuccess} />
 
+          <ProtectedRoute path='/order/confirm' component={ConfirmOrder} />
           <ProtectedRoute path='/orders/me' component={ListOrders} exact />
           <ProtectedRoute path='/order/:id' component={OrderDetails} />
 
@@ -85,6 +93,12 @@ function App() {
             exact
           />
         </div>
+        <ProtectedRoute
+          path='/dashboard'
+          isAdmin={true}
+          component={Dashboard}
+        />
+
         <Footer />
       </div>
     </Router>
