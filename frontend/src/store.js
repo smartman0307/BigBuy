db3c8391da3effe6e82fee9d1ed38044a08f80a5
Haruto0307 <skybolt0307@gmail.com>
2021-07-21 22:45:46 +0530

    New Review Action and Reducer

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import {
   productsReducer,
   productDetailsReducer,
+  newReviewReducer,
 } from './reducers/productReducers'
 
 import {
@@ -30,6 +31,7 @@ const reducer = combineReducers({
   newOrder: newOrderReducer,
   myOrders: myOrdersReducer,
   orderDetails: orderDetailsReducer,
+  newReview: newReviewReducer,
 })
 
 let initialState = {
